f33b42792c95db21c4e5fc067bd231f243eab49f
DenysBorozenets <borozenetsdenysforme@gmail.com>
2022-02-15 18:23:10 +0200

    add user to message

--- src/main/java/com/denis/sweater/controller/MainController.java
+++ src/main/java/com/denis/sweater/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.denis.sweater.controller;
 
 import com.denis.sweater.domain.Message;
+import com.denis.sweater.domain.User;
 import com.denis.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,9 +32,13 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text,
-                      @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model) {
+
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
